7b02c3bf593ed5677a1b2d5929da3997f7b7cef2
Rishi Kumar <kumarrishi@Rishis-MacBook-Air.local>
2025-01-07 10:26:36 +0530

    repo added

---
+++ backend/src/main/java/com/alphadev/alphaHotel/repo/BookingRepository.java
@@ -0,0 +1,17 @@
+package com.alphadev.alphaHotel.repo;
+
+import com.alphadev.alphaHotel.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    List<Booking> findByRoomId(Long roomId);
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+
+    List<Booking> findByUserId(Long userId);
+
+}
